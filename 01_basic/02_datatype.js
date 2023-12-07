aca3cc3ec9cf216f1ab621a12b480c59f69799ec
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2023-12-07 16:28:19 +0000

    data type

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert(6+6)  // we are using node js not browser
+
+console.log(3
+    +
+    3) //this type of style not use to write a code because code readability shund be high
+
+console.log("rajiv")
+
+let name="rajiv"
+let age= 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+// string=>""
+// boolean => true / false
+//null=> standalone value
+// undefined =>
+// symbol => unique 
+
+//object  
+
+console.log(typeof undefined); // show the data type
+console.log(typeof null); // null is a object
