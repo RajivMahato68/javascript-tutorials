4b12a0dc1a74e8930e49fab389a0dc0ecae56bc2
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2023-12-09 16:03:52 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score=false
+
+//console.log(typeof score); //check variable type in this method
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=33
+//"33abc"=>NaN
+//true=>1; false=>0; 
+
+
+let isLoggedIn = "rajiv"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true;  0=> false
+// "" => false
+// "rajiv" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
