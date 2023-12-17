6475e9254cbb47d7d8ae3b2318ce9176f4277e61
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2023-12-17 22:08:33 +0545

    function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -31,5 +31,35 @@ return
     }
     return `${userName} just logged in`
 }
-console.log(loginUserMessage("hello"));
+// console.log(loginUserMessage("hello"));
 // console.log(loginUserMessage("Rajiv"))
+
+
+
+
+function calculateCartPrice(...num1){
+return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    userName: "Rajiv",
+    price: 166
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.userName}and price is ${anyobject.price}`);
+}
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
