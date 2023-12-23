fbb10e01faa06443903e807078c87d8e4a0f0bee
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2023-12-23 12:23:48 +0545

    1st project solution

--- 07_project/chaiaurcode.js
+++ 07_project/chaiaurcode.js
@@ -4,20 +4,40 @@
  buttons.forEach(function(button){
     console.log(button);
     button.addEventListener('click',function(event){
-       console.log(event); 
-       console.log(event.target); 
-        if(event.target.id === 'gray'){
+    //    console.log(event); 
+    //    console.log(event.target); 
+    //     if(event.target.id === 'gray'){
+    //     body.style.backgroundColor = event.target.id;
+    //     }
+    //     if(event.target.id === 'white'){
+    //         body.style.backgroundColor = event.target.id;
+    //         }
+    //         if(event.target.id === 'blue'){
+    //             body.style.backgroundColor = event.target.id;
+    //             }
+    //             if(event.target.id === 'yellow'){
+    //                 body.style.backgroundColor = event.target.id;
+    //                 }
+
+// Switch case using 
+
+switch (event.target.id) {
+    case 'gray':
         body.style.backgroundColor = event.target.id;
-        }
-        if(event.target.id === 'white'){
-            body.style.backgroundColor = event.target.id;
-            }
-            if(event.target.id === 'blue'){
-                body.style.backgroundColor = event.target.id;
-                }
-                if(event.target.id === 'yellow'){
-                    body.style.backgroundColor = event.target.id;
-                    }
+        break;
+    case 'white':
+        body.style.backgroundColor = event.target.id;
+        break;
+    case 'blue':
+        body.style.backgroundColor = event.target.id;
+        break;
+    case 'yellow':
+        body.style.backgroundColor = event.target.id;
+        break;
+    default:
+    
+        break;
+}
 
     })
     
